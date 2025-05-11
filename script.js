a7b89ccbb5311cb9a6b958eaeca6d83e001215c6
MHRezaie <rezaiee.mhr@gmail.com>
2025-05-11 16:55:28 +0200

    add script file

---
+++ script.js
@@ -0,0 +1,12 @@
+class mycalss {
+    constructor(name="tmp") {
+      this.name = name;
+    }
+    greet() {
+      //console.log(`Hello, ${this.name}!`);
+      alert(origin);
+    }
+  }
+  // Expose it globally
+  window.mycalss = myclass;
+  
